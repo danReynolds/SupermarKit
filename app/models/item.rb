c98590283e70468f1d43f78b0f02773480274f8a
danReynolds <fnetma@hotmail.com>
2014-12-16 23:12:31 -0500

    added new database config, style changes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,5 @@
 class Item < ActiveRecord::Base
 	belongs_to :grocery
 
-  scope :search, ->(q) { where('NAME LIKE :name OR ID LIKE :name', name: "%#{q}%") }
+  scope :search, ->(q) { where('NAME LIKE :query OR ID LIKE :query', query: "%#{q}%") }
 end
