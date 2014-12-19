d9b67485bf310590ff8117bad260ad4965d16a80
danReynolds <fnetma@hotmail.com>
2014-12-18 22:41:38 -0500

    finished select2, works great

--- app/controllers/groceries_controller.rb
+++ app/controllers/groceries_controller.rb
@@ -18,12 +18,25 @@ class GroceriesController < ApplicationController
 	end
 
 	def auto_complete
-		raise
+		grocery = Grocery.find(params[:id])
+		items = current_user.items.with_name(params[:q]) - grocery.items
+		items.map do |item|
+		  {
+				id: item.id,
+			 	name: item.name,
+				description: item.description
+			}
+		end
+
+		render json: {
+			total_items: items.count,
+			items: items
+		}
 	end
 
 	def add_items
 		grocery = Grocery.find(params[:id])
-		items = Item.find(params[:grocery][:name][1..-1])
+		items = Item.find(params[:grocery][:name].split(",")[1..-1])
 		grocery.items << items
 		render json: { success: true }
 	end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ActiveRecord::Base
 	belongs_to :grocery
+
+  scope :with_name, ->(q) { where('items.name LIKE ?', "%#{q}%") }
 end
