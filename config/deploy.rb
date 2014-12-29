ecb314bb80789564a615af9e7c9d9e2822a0bee4
danReynolds <fnetma@hotmail.com>
2014-12-29 16:57:19 -0500

    update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,7 @@ set :application, 'softgroceries'
 set :repo_url, 'git@github.com:danReynolds/softgroceries.git'
 set :deploy_to, '/home/deploy/softgroceries'
 set :linked_files, %w{config/database.yml config/secrets.yml config/.env}
+set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
@@ -27,7 +28,6 @@ set :linked_files, %w{config/database.yml config/secrets.yml config/.env}
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('bin', 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
