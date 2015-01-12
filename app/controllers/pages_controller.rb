7b6a83979312f7581ca0f1fd8af9c6138f39480e
danReynolds <fnetma@hotmail.com>
2015-01-12 14:14:30 -0500

    add an about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,4 +4,7 @@ class PagesController < ApplicationController
 
   def home
   end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   get 'password_resets/create'
   get 'password_resets/edit'
   get 'password_resets/update'
-  get 'login' => 'user_sessions#new', :as => :login
-  post 'logout' => 'user_sessions#destroy', :as => :logout
+  get 'login' => 'user_sessions#new', as: :login
+  post 'logout' => 'user_sessions#destroy', as: :logout
+  get 'about' => 'pages#about', as: :about
   root to: 'pages#home'
   resources :password_resets
   resources :user_sessions
