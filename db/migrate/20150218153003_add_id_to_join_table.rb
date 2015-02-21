fb7645d462c322a2c3159c99f19d2e2206c93e37
danReynolds <fnetma@hotmail.com>
2015-02-21 01:13:45 -0500

    update migrations

--- db/migrate/20150218153003_add_id_to_join_table.rb
+++ db/migrate/20150218153003_add_id_to_join_table.rb
@@ -1,5 +1,6 @@
 class AddIdToJoinTable < ActiveRecord::Migration
   def change
     add_column :groceries_items, :id, :primary_key
+    add_column :groceries_items, :quantity, :integer, default: 1
   end
 end
--- db/migrate/20150221035430_change_groceries_item_default_quantity.rb
+++
@@ -1,5 +0,0 @@
-class ChangeGroceriesItemDefaultQuantity < ActiveRecord::Migration
-  def change
-    change_column :groceries_items, :quantity, :integer, default: 1
-  end
-end
