136afe640bd4913c3b1c8ba4c83768fa94587d3d
danReynolds <fnetma@hotmail.com>
2015-04-30 17:48:42 -0400

    Invites work, need additional functionality

--- db/schema.rb
+++ db/schema.rb
@@ -27,7 +27,6 @@ ActiveRecord::Schema.define(version: 20150430013216) do
     t.string   "uid",        null: false
     t.datetime "created_at"
     t.datetime "updated_at"
-  end
 
   create_table "groceries", force: true do |t|
     t.string   "name"
