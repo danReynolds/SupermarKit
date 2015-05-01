e1274327513cc66ebee4d7710c78290cbc84ae0b
danReynolds <fnetma@hotmail.com>
2015-05-01 09:12:12 -0400

    Added Oauth, near complete

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20150430013216) do
     t.datetime "updated_at"
   end
 
+  create_table "authentications", force: true do |t|
+    t.integer  "user_id",    null: false
+    t.string   "provider",   null: false
+    t.string   "uid",        null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "groceries", force: true do |t|
     t.string   "name"
     t.string   "description"
