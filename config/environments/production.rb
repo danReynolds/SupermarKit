cf72005aaf6b2b44aca550794742a3701fbc27df
danReynolds <fnetma@hotmail.com>
2015-05-28 09:17:15 -0400

    try turning off compression

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "cloudfront.supermarkit.ca"
+  # config.action_controller.asset_host = "cloudfront.supermarkit.ca"
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
