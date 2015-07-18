3b5a559fa2fa1ba773eefe2e9ad00f58bfbd512f
danReynolds <fnetma@hotmail.com>
2015-07-18 00:15:10 -0400

    add basic grocery store table

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
 
-  post "oauth/callback" => "oauths#callback"
-  get "oauth/callback" => "oauths#callback" # for use with Github, Facebook
-  get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
+  post 'oauth/callback' => 'oauths#callback'
+  get 'oauth/callback' => 'oauths#callback' # for use with Github, Facebook
+  get 'oauth/:provider' => 'oauths#oauth', :as => :auth_at_provider
 
   get 'login' => 'user_sessions#new', as: :login
   post 'logout' => 'user_sessions#destroy', as: :logout
@@ -21,7 +21,8 @@ Rails.application.routes.draw do
     end
   end
 
-  shallow do # Only the collection routes of the children get member routes of the parent
+  # Only the collection routes of the children get member routes of the parent
+  shallow do
     resources :user_groups do
       resources :groceries do
         resources :items do
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150701015115) do
+ActiveRecord::Schema.define(version: 20150718035953) do
 
   create_table "authentications", force: true do |t|
     t.integer  "user_id"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 20150701015115) do
     t.integer "item_id",                null: false
     t.integer "grocery_id",             null: false
     t.integer "quantity",   default: 1
+  end
+
+  create_table "grocery_stores", force: true do |t|
+    t.string  "name"
+    t.decimal "lat",  precision: 10, scale: 6
+    t.decimal "lng",  precision: 10, scale: 6
   end
 
   create_table "items", force: true do |t|
