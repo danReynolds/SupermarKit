2006d6a5d352d9514dfe3c89ee5e46fc1c063854
danReynolds <fnetma@hotmail.com>
2015-07-18 22:27:32 -0400

    appease hound

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,114 +4,114 @@ class ItemsController < ApplicationController
   load_and_authorize_resource :grocery
   load_and_authorize_resource :item, through: :grocery, shallow: true
 
-	def index
-		respond_to do |format|
-			format.json do
-		    items = @items.map do |item|
-		      {
-		      	id: item.id,
-		      	name: item.name,
-		      	description: item.description.to_s,
-		      	grocery_item_id: item.grocery_item(@grocery).id,
-		      	quantity: item.quantity(@grocery),
-		      	price: item.price(@grocery).dollars.to_s,
-		      	price_formatted: item.price(@grocery).format,
-		      	total_price_formatted: item.total_price(@grocery).format,
-		      	path: item_path(item.id)
-		      }
-		    end
-    		render json: { data: items }
-		  end
+  def index
+    respond_to do |format|
+      format.json do
+        items = @items.map do |item|
+          {
+            id: item.id,
+            name: item.name,
+            description: item.description.to_s,
+            grocery_item_id: item.grocery_item(@grocery).id,
+            quantity: item.quantity(@grocery),
+            price: item.price(@grocery).dollars.to_s,
+            price_formatted: item.price(@grocery).format,
+            total_price_formatted: item.total_price(@grocery).format,
+            path: item_path(item.id)
+          }
+        end
+        render json: { data: items }
+      end
     end
-	end
+  end
 
-	def show
-		@user_group = @item.groceries.first.user_group
-	end
+  def show
+    @user_group = @item.groceries.first.user_group
+  end
 
-	def new
-		@groceries_items = @item.groceries_items.new
-	end
+  def new
+    @groceries_items = @item.groceries_items.new
+  end
 
-	def create
-		@item = Item.new(item_params)
-		@item.name.capitalize!
+  def create
+    @item = Item.new(item_params)
+    @item.name.capitalize!
 
-		if @item.save
-			redirect_to @grocery
-		else
+    if @item.save
+      redirect_to @grocery
+    else
       render action: :new, alert: 'Unable to create new item.'
-		end
-	end
+    end
+  end
 
-	def edit
-		@user_group = @item.groceries.first.user_group
-	end
+  def edit
+    @user_group = @item.groceries.first.user_group
+  end
 
-	def update
-		respond_to do |format|
-			format.json do
-				if @item.update_attributes(item_params)
-					render json: {}, status: :ok
-				else
-					render nothing: true, status: :internal_server_error
-				end
-			end
+  def update
+    respond_to do |format|
+      format.json do
+        if @item.update_attributes(item_params)
+          render json: {}, status: :ok
+        else
+          render nothing: true, status: :internal_server_error
+        end
+      end
 
-			format.html do
-				if @item.update_attributes(item_params)
-					redirect_to @item.groceries.first.user_group
-				else
-					render :edit
-				end
-			end
-		end
-	end
+      format.html do
+        if @item.update_attributes(item_params)
+          redirect_to @item.groceries.first.user_group
+        else
+          render :edit
+        end
+      end
+    end
+  end
 
-	def auto_complete
-		items = @grocery.user_group.privacy_items.with_name(params[:q]) - @grocery.items
-		items = items.first(5).map do |item|
-		  {
-				id: item.id,
-			 	name: item.name,
-				description: item.description
-			}
-		end
+  def auto_complete
+    items = @grocery.user_group.privacy_items.with_name(params[:q]) - @grocery.items
+    items = items.first(5).map do |item|
+      {
+        id: item.id,
+         name: item.name,
+        description: item.description
+      }
+    end
 
-		render json: {
-			total_items: items.count,
-			items: items
-		}
-	end
+    render json: {
+      total_items: items.count,
+      items: items
+    }
+  end
 
-	def add
+  def add
     # Id is either an id of a known item or a name for a new one
-		params[:items][:ids].split(",").each do |id|
-			item = Item.find_by_id(id) || Item.create(name: id)
+    params[:items][:ids].split(',').each do |id|
+      item = Item.find_by_id(id) || Item.create(name: id)
 
       @grocery.items << item
       groceries_item = item.grocery_item(@grocery)
       groceries_item.price_cents = groceries_item.localized_price
       groceries_item.save!
-		end
+    end
 
     render nothing: true, status: :ok
-	end
+  end
 
-	def remove
-		# the grocery is already loaded because grocery_id was passed with the request
-		# canard picks up on the grocery_id being passed when using load_resource
-		@grocery.items.delete(@item)
+  def remove
+    # the grocery is already loaded because grocery_id was passed with the request
+    # canard picks up on the grocery_id being passed when using load_resource
+    @grocery.items.delete(@item)
     render nothing: true, status: :ok
-	end
+  end
 
 private
-	def item_params
-		params.require(:item).permit(
+  def item_params
+    params.require(:item).permit(
       :name,
       :description,
       groceries_items_attributes:
         [:price, :price_cents, :id, :quantity, :grocery_id]
     )
-	end
+  end
 end
