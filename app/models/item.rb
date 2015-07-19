77be210cf4ff9ad96394aaac83cec79792a8a10c
danReynolds <fnetma@hotmail.com>
2015-07-18 22:27:32 -0400

    appease hound

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,27 +1,27 @@
 class Item < ActiveRecord::Base
 
-	has_many :groceries_items, class_name: GroceriesItems, inverse_of: :item
-	has_many :groceries, through: :groceries_items
+  has_many :groceries_items, class_name: GroceriesItems, inverse_of: :item
+  has_many :groceries, through: :groceries_items
 
-	validates :name, presence: true
+  validates :name, presence: true
 
   accepts_nested_attributes_for :groceries_items
 
-	validates :name, presence: true
+  validates :name, presence: true
 
   scope :with_name, ->(q) { where('items.name LIKE ?', "%#{q}%").distinct }
 
-	def grocery_item(grocery)
-		groceries_items.find_by_grocery_id(grocery.id)
-	end
+  def grocery_item(grocery)
+    groceries_items.find_by_grocery_id(grocery.id)
+  end
 
-	def quantity(grocery)
-		grocery_item(grocery).quantity
-	end
+  def quantity(grocery)
+    grocery_item(grocery).quantity
+  end
 
-	def price(grocery)
-		grocery_item(grocery).price
-	end
+  def price(grocery)
+    grocery_item(grocery).price
+  end
 
   def total_price(grocery)
     quantity(grocery) * price(grocery)
