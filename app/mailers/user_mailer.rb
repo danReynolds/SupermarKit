75157887c4788d90460547279f2deb490622089a
danReynolds <fnetma@hotmail.com>
2015-07-18 22:42:06 -0400

    remove user mailer password reset

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,6 @@
 class UserMailer < ActionMailer::Base
   default from: 'us@supermarkit.ca'
 
-  def reset_password_email(user)
-    @user = user
-    @url  = edit_password_reset_url(user.reset_password_token)
-    mail(to: user.email, subject: 'Your password has been reset')
-  end
-
   def activation_needed_email(user)
     @user = user
     @url = activate_user_path(@user)
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe UserMailer, type: :mailer do
   let(:user) { create(:user) }
 
-  describe "activation_needed_email" do
+  describe 'activation_needed_email' do
     let(:mail) { UserMailer.activation_needed_email(user) }
 
-    it "renders the headers" do
+    it 'renders the headers' do
       expect(mail.subject).to eq('Join Supermarkit')
       expect(mail.to).to eq(["#{user.email}"])
       expect(mail.from).to eq(["us@supermarkit.ca"])
     end
   end
 
-  describe "activation_success_email" do
+  describe 'activation_success_email' do
     let(:mail) { UserMailer.activation_success_email(user) }
 
     it "renders the headers" do
@@ -23,4 +23,14 @@ RSpec.describe UserMailer, type: :mailer do
     end
   end
 
+  describe 'send_grocery_list_email' do
+    let(:grocery) { create(:grocery) }
+    let(:mail) { UserMailer.send_grocery_list_email(user, grocery) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Groceries For #{grocery.name}")
+      expect(mail.to).to eq(["#{user.email}"])
+      expect(mail.from).to eq(["us@supermarkit.ca"])
+    end
+  end
 end
