0a749c24ce942891cc91e9540416cad15790abb4
danReynolds <fnetma@hotmail.com>
2015-07-18 20:11:09 -0400

    cleaned up the groceries js prior to splitting up

--- app/models/groceries_items.rb
+++ app/models/groceries_items.rb
@@ -2,7 +2,6 @@ class GroceriesItems < ActiveRecord::Base
   belongs_to :item
   belongs_to :grocery
 
-  validates :name, presence: true
 	validates :price, numericality: { greater_than_or_equal_to: 0 }
   monetize :price_cents
 
