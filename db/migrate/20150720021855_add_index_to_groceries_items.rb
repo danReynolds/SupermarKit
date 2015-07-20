73553a39d2424823cc02c598e40ca1a5f27abbf6
danReynolds <fnetma@hotmail.com>
2015-07-19 23:14:08 -0400

    add missing indices for join tables

--- db/migrate/20150720021855_add_index_to_groceries_items.rb
+++ db/migrate/20150720021855_add_index_to_groceries_items.rb
@@ -1,5 +1,6 @@
 class AddIndexToGroceriesItems < ActiveRecord::Migration
   def change
     add_index :groceries_items, [:item_id, :grocery_id], unique: true
+    add_index :groceries_items, :grocery_id
   end
 end
