2309c69ed9029631d84bc8df305aca5effe780e9
danReynolds <fnetma@hotmail.com>
2015-07-19 23:14:08 -0400

    add missing indices for join tables

--- db/migrate/20150720022859_add_index_to_user_groups_users.rb
+++ db/migrate/20150720022859_add_index_to_user_groups_users.rb
@@ -1,5 +1,6 @@
 class AddIndexToUserGroupsUsers < ActiveRecord::Migration
   def change
     add_index :user_groups_users, [:user_id, :user_group_id], unique: true
+    add_index :user_groups_users, :user_group_id
   end
 end
