c2665b2692b92386b25cec7f2419a73767b1c7d1
danReynolds <fnetma@hotmail.com>
2015-11-12 10:50:47 -0500

    try puma ssl option

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,6 +25,7 @@ set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
 set :puma_init_active_record, true  # Change to false when not using ActiveRecord
+set :nginx_use_ssl, true
 
 ## Defaults:
 # set :scm,           :git
