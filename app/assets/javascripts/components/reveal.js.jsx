ea5e4f4ee2dd947f6568a91965c2b044ad38efd5
danReynolds <fnetma@hotmail.com>
2016-02-21 09:10:50 -0800

    switch modal to be rendered within reveal

--- app/assets/javascripts/components/reveal.js.jsx
+++ app/assets/javascripts/components/reveal.js.jsx
@@ -77,28 +77,32 @@ var Reveal = React.createClass({
     },
 
     handleSave: function() {
-        $(this.props.modal).closeModal();
-        this.dispatchSelection();
+        $(document.getElementById(this.props.modal)).closeModal();
+        if (this.props.dispatchReceiver) {
+            this.dispatchSelection();
+        }
     },
 
     addToSelection: function(index) {
         this.setState({
             backspaceTarget: null,
             scrollTarget: 0,
-            selection: this.state.selection.concat(this.state.results[index])
+            selection: this.state.selection.concat(this.state.results[index]),
+            results: React.addons.update(this.state.results, {$splice: [[index, 1]]})
         });
     },
 
     removeFromSelection: function(index) {
         this.setState({
             backspaceTarget: null,
-            selection: React.addons.update(this.state.selection, {$splice: [[index, 1]]})
+            selection: React.addons.update(this.state.selection, {$splice: [[index, 1]]}),
+            results: React.addons.update(this.state.results, {$push: [this.state.selection[index]]})
         });
     },
 
     dispatchSelection: function() {
         var event = new CustomEvent('selection-updated', { detail: this.state.selection });
-        document.querySelector('.multiselect').dispatchEvent(event);
+        document.querySelector(this.props.dispatchReceiver).dispatchEvent(event);
     },
 
     getResults: function(query) {
@@ -139,11 +143,7 @@ var Reveal = React.createClass({
         var self = this;
         var resultClass = 'valign-wrapper';
 
-        var results = this.state.results.filter(function(result) {
-            return !self.state.selection.map(function(result) {
-                return result.id;
-            }).includes(result.id);
-        }).map(function(result, index) {
+        var results = this.state.results.map(function(result, index) {
             return (
                 React.createElement(this[self.props.type], {
                     key: "result-" + index,
@@ -156,37 +156,39 @@ var Reveal = React.createClass({
         });
 
         return (
-            <div className='reveal'>
-                <nav>
-                    <div className='nav-wrapper'>
-                        <form>
-                            <div className='input-field'>
-                                <input
-                                    autoComplete='off'
-                                    id='search'
-                                    type='search'
-                                    ref='search'
-                                    value={this.state.value}
-                                    onKeyDown={this.handleKeyPress}
-                                    onChange={this.handleChange}
-                                    required />
-                                <label htmlFor='search'><i className='material-icons'>search</i></label>
-                                <i className='material-icons'>close</i>
-                          </div>
-                        </form>
+            <div id='change-members' className='modal bottom-sheet'>
+                <div className='reveal'>
+                    <nav>
+                        <div className='nav-wrapper'>
+                            <form>
+                                <div className='input-field'>
+                                    <input
+                                        autoComplete='off'
+                                        id='search'
+                                        type='search'
+                                        ref='search'
+                                        value={this.state.value}
+                                        onKeyDown={this.handleKeyPress}
+                                        onChange={this.handleChange}
+                                        required />
+                                    <label htmlFor='search'><i className='material-icons'>search</i></label>
+                                    <i className='material-icons'>close</i>
+                              </div>
+                            </form>
+                        </div>
+                    </nav>
+                    <Multiselect
+                        ref="selection"
+                        selection={this.state.selection}
+                        backspaceTarget={this.state.backspaceTarget}
+                        removeFromSelection={this.removeFromSelection}/>
+                    <ul className='results-container'>
+                        {results}
+                    </ul>
+                    <div className='reveal-controls'>
+                        <a className='waves-effect waves-light btn cancel modal-close'><i className='material-icons left'>close</i>Cancel</a>
+                        <a className='waves-effect waves-light btn' onClick={this.handleSave}><i className='material-icons left'>send</i>Update</a>
                     </div>
-                </nav>
-                <Multiselect
-                    ref="selection"
-                    selection={this.state.selection}
-                    backspaceTarget={this.state.backspaceTarget}
-                    removeFromSelection={this.removeFromSelection}/>
-                <ul className='results-container'>
-                    {results}
-                </ul>
-                <div className='reveal-controls'>
-                    <a className='waves-effect waves-light btn cancel modal-close'><i className='material-icons left'>close</i>Cancel</a>
-                    <a className='waves-effect waves-light btn' onClick={this.handleSave}><i className='material-icons left'>send</i>Update</a>
                 </div>
             </div>
         );
