3aafc1ab862a3617b59c22644b3dd94900ac504a
danReynolds <fnetma@hotmail.com>
2016-04-09 16:21:51 -0700

    updated tests for items controller

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,18 +1,8 @@
 require 'rails_helper'
 require 'support/basic_user'
-require 'support/routes'
 
 RSpec.describe ItemsController, type: :controller do
   include_context 'basic user'
-
-  let(:id) { grocery.items.first.id }
-  let(:grocery_id) { grocery.id }
-  it_should_behave_like 'routes', {
-    new: { grocery_id: true },
-    show: { id: true },
-    edit: { id: true },
-    create: { method: :post, grocery_id: true }
-  }
 
   describe 'GET index' do
     subject { get :index, grocery_id: grocery, format: :json }
@@ -102,38 +92,30 @@ RSpec.describe ItemsController, type: :controller do
   end
 
   describe 'GET auto_complete' do
-    describe 'Scope by presence' do
-      it 'returns an item not present in current grocery list' do
-        items = user_group.items - grocery.items
-        get :auto_complete, grocery_id: grocery, q: items.first.name
-        resp = JSON.parse(response.body)['total_items']
-        expect(resp).to eq 1
-      end
+    let(:data) { JSON.parse(response.body)['data'] }
 
-      it 'does not return an item present in the current grocery list' do
-        item = grocery.items.first
-        get :auto_complete, grocery_id: grocery, q: item.name
-        resp = JSON.parse(response.body)['total_items']
-        expect(resp).to eq 0
-      end
+    it 'returns an item matching the search query' do
+      item = grocery.items.first
+      get :auto_complete, grocery_id: grocery, q: item.name
+
+      expect(data.length).to eq 1
+      expect(data.first['name']).to eq item.name
     end
 
-    describe 'Scope by privacy' do
+    describe 'scope by privacy' do
       context 'public kit' do
         it 'returns other public group items' do
           group = create(:user_group, :with_groceries)
           item = group.items.first
           get :auto_complete, grocery_id: grocery, q: item.name
-          resp = JSON.parse(response.body)['total_items']
-          expect(resp).to eq 1
+          expect(data.length).to eq 1
         end
 
         it 'does not return other private group items' do
           group = create(:user_group, :with_groceries, privacy: UserGroup::PRIVATE)
           item = group.items.first
           get :auto_complete, grocery_id: grocery, q: item.name
-          resp = JSON.parse(response.body)['total_items']
-          expect(resp).to eq 0
+          expect(data.length).to eq 0
         end
       end
 
@@ -144,8 +126,7 @@ RSpec.describe ItemsController, type: :controller do
           item = other_group.items.first
 
           get :auto_complete, grocery_id: grocery, q: item.name
-          resp = JSON.parse(response.body)['total_items']
-          expect(resp).to eq 0
+          expect(data.length).to eq 0
         end
 
         it 'returns own private group items' do
@@ -153,90 +134,9 @@ RSpec.describe ItemsController, type: :controller do
           items = user_group.items - grocery.items
 
           get :auto_complete, grocery_id: grocery, q: items.first.name
-          resp = JSON.parse(response.body)['total_items']
-          expect(resp).to eq 1
+          expect(data.length).to eq 1
         end
       end
-    end
-  end
-
-  describe 'PATCH add' do
-    let(:new_item_name) { 'Schnitzel' }
-    let(:item) { create(:item) }
-
-    context 'new item' do
-      subject { patch :add, grocery_id: grocery, items: { ids: [new_item_name] } }
-      it 'should create the new item' do
-        expect { subject }.to change(Item, :count).by(1)
-      end
-
-      it 'should set the new item price to zero' do
-        subject
-        expect(Item.last.grocery_item(grocery).price_cents).to eq 0
-      end
-    end
-
-    context 'existing item' do
-      subject { patch :add, grocery_id: grocery, items: { ids: [item.id] } }
-
-      context 'without a store' do
-        before(:each) do
-          groceries = create_list :grocery, 3, items: [item]
-          item.grocery_item(groceries[0]).update_attribute(:price_cents, 500)
-          item.grocery_item(groceries[1]).update_attribute(:price_cents, 100)
-          item.grocery_item(groceries[2]).update_attribute(:price_cents, 500)
-        end
-
-        it 'should assign the overall most common price' do
-          subject
-          expect(item.reload.grocery_item(grocery).price_cents).to eq 500
-        end
-      end
-
-      context 'with a store' do
-        let(:nearby_store) { create(:grocery_store) }
-        before(:each) do
-          grocery.update_attribute(:grocery_store, nearby_store)
-          groceries = create_list :grocery, 3, items: [item], grocery_store: nearby_store
-          item.grocery_item(groceries[0]).update_attribute(:price_cents, 500)
-          item.grocery_item(groceries[1]).update_attribute(:price_cents, 100)
-          item.grocery_item(groceries[2]).update_attribute(:price_cents, 500)
-
-          other_groceries = create_list :grocery, 3, items: [item]
-          other_groceries.each do |grocery|
-            item.grocery_item(grocery).update_attribute(:price_cents, 50)
-          end
-        end
-
-        context 'with a nearby store' do
-          it 'should assign the most common price from the nearby store' do
-            subject
-            expect(item.reload.grocery_item(grocery).price_cents).to eq 500
-          end
-        end
-
-        context 'without a nearby store' do
-          let(:nearby_store) { nil }
-          it 'should fallback on the general most common price' do
-            subject
-            expect(item.reload.grocery_item(grocery).price_cents).to eq 50
-          end
-        end
-      end
-    end
-  end
-
-  describe 'PATCH remove' do
-    let(:item) { grocery.items.last }
-    subject { patch :remove, grocery_id: grocery, id: item }
-
-    it 'removes the item from grocery' do
-      subject
-      expect(grocery.reload.items).not_to include(item)
-    end
-
-    it 'successfully returns' do
-      expect(subject).to be_ok
     end
   end
 end
