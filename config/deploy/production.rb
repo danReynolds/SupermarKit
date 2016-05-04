6323142dc523535c2e9cdca2646f15af12e4786d
danReynolds <fnetma@hotmail.com>
2016-05-03 23:38:41 -0400

    wrap prod paperclip config in application configure

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -60,11 +60,13 @@
 #     # password: 'please use keys'
 #   }
 
-config.paperclip_defaults = {
-  storage: :s3,
-  s3_credentials: {
-    bucket: ENV['S3_BUCKET_NAME'],
-    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+Rails.application.configure do
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV['S3_BUCKET_NAME'],
+      access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+    }
   }
-}
+end
