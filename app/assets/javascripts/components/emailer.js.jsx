940da1681e768fa94be57686056945f12d5c739a
danReynolds <fnetma@hotmail.com>
2016-05-09 12:49:23 -0400

    update grocery list send email

--- app/assets/javascripts/components/emailer.js.jsx
+++ app/assets/javascripts/components/emailer.js.jsx
@@ -1,13 +1,25 @@
 var Emailer = React.createClass({
     mixins: [ModalContainer],
 
+    getInitialState: function() {
+        return {
+            message: ''
+        }
+    },
+
+    handleChange: function(e) {
+        this.setState({
+            message: e.target.value
+        });
+    },
+
     render: function() {
-        return (
-            <div className='card emailer'>
-                <div className='card-content full-width dark'>
-                    <div className='card-header'>
-                        <h3>Email Members</h3>
-                    </div>
+        var content;
+        if (this.state.modal.open) {
+            content = <Loader />;
+        } else {
+            content = (
+                <div>
                     <div className='email-users'>
                         <Multiselect
                             buttonText={this.props.buttonText}
@@ -17,6 +29,8 @@ var Emailer = React.createClass({
                     <div className='row'>
                         <div className="input-field col s12">
                             <textarea
+                                value={this.state.message}
+                                onChange={this.handleChange}
                                 placeholder="Message for Kit members"
                                 id="textarea1"
                                 className="materialize-textarea">
@@ -25,35 +39,49 @@ var Emailer = React.createClass({
                     </div>
                     <div className='card-action'>
                         <a
-                            onClick={this.handleSave}
+                            onClick={this.deliverEmail}
                             className='waves-effect waves-light btn'>
                             <i className='material-icons left'>send</i>
                             Send
                         </a>
                     </div>
                 </div>
-
+            );
+        }
+        return (
+            <div className='card emailer'>
+                <div className='card-content full-width dark'>
+                    <div className='card-header'>
+                        <h3>Email Members</h3>
+                    </div>
+                    {content}
+                </div>
                 <Modal
                     {...this.state.modal}
                     {...this.props.modal}/>
             </div>
-        )
+        );
     },
 
-    handleSave: function() {
+    deliverEmail: function() {
         $.ajax({
             method: 'POST',
             data: JSON.stringify({
                 grocery: {
-                    email: this.state.modal.selection.map(function(item) {
-                        return {
-                            user_id: item.id
-                        }
-                    })
+                    email: {
+                        user_ids: this.state.modal.selection.map(function(user) {
+                            return user.id;
+                        ),
+                        message: this.state.message
+                    }
                 }
             }),
             contentType: 'application/json',
             url: this.props.url
         });
+    },
+
+    handleSave: function() {
+        this.toggleModal();
     }
 });
