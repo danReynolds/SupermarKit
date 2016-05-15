5dd67e8af31c1e8c6ec8a8986f8f733d39432114
danReynolds <fnetma@hotmail.com>
2016-05-15 13:00:22 -0400

    show your recipes if you have any, if not show suggested

--- app/assets/javascripts/components/recipe_result.js.jsx
+++ app/assets/javascripts/components/recipe_result.js.jsx
@@ -3,28 +3,43 @@ var RecipeResult = React.createClass({
 
     propTypes: {
         resultIndex: React.PropTypes.number.isRequired,
-        handleAdd: React.PropTypes.func.isRequired,
+        handleAdd: React.PropTypes.func,
         result: React.PropTypes.object.isRequired,
-        scrollTarget: React.PropTypes.number.isRequired
+        scrollTarget: React.PropTypes.number
+    },
+
+    handleClick: function(e) {
+        e.stopPropagation();
     },
 
     render: function() {
-        var resultClass = 'valign-wrapper recipe-result' + (this.props.resultIndex == this.props.scrollTarget ? ' target' : "");
+        var resultClass = 'valign-wrapper recipe-result' + (this.props.resultIndex === this.props.scrollTarget ? ' target' : "");
+        var instructionsLink;
+        
+        if (this.props.result.url) {
+            instructionsLink = (
+                <a
+                    onClick={this.handleClick}
+                    target='_blank'
+                    href={this.props.result.url}>
+                    View
+                </a>
+            );
+        }
         return (
             <li
                 className={resultClass}
                 onClick={this.props.handleAdd}
                 data-index={this.props.resultIndex}>
-                <div className='valign-wrapper'>
-                    <img src={this.props.result.image}/>
-                    <div className='info'>
-                        <p>{this.props.result.name}</p>
-                        <p>{this.props.result.time}</p>
-                        <div className='ratings'>
-                            {this.renderRatings(this.props.result.rating)}
-                        </div>
+                <img src={this.props.result.image}/>
+                <div className='info'>
+                    <p>{this.props.result.name}</p>
+                    <p>{this.props.result.time}</p>
+                    <div className='ratings'>
+                        {this.renderRatings(this.props.result.rating)}
                     </div>
                 </div>
+                {instructionsLink}
             </li>
         );
     }
