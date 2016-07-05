cc1ebc0e4aad9674b5081e513d45f4732ad4ce6b
danReynolds <fnetma@hotmail.com>
2016-07-05 14:35:49 -0400

    fixed schema

--- db/migrate/20160705154548_add_payeeand_payer_to_user_payments.rb
+++
@@ -1,7 +0,0 @@
-class AddPayeeandPayerToUserPayments < ActiveRecord::Migration
-  def change
-    remove_column :user_payments, :user_id
-    add_column :user_payments, :payee_id, :integer
-    add_column :user_payments, :payer_id, :integer
-  end
-end
--- db/migrate/20160705162705_remove_sti_not_appropriate.rb
+++
@@ -1,9 +0,0 @@
-class RemoveStiNotAppropriate < ActiveRecord::Migration
-  def change
-    drop_table :payments
-    add_column :grocery_payments, :price_cents, :integer, default: 0
-    add_column :user_payments, :price_cents, :integer, default: 0
-    add_index :user_payments, :payee_id
-    add_index :user_payments, :payer_id
-  end
-end
