9d7f9542a238df64ff11881e3e1e22e393a39abe
danReynolds <fnetma@hotmail.com>
2016-07-06 12:21:03 -0400

    rearranged tests for user group user

--- spec/models/user_groups_users_spec.rb
+++ spec/models/user_groups_users_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe UserGroupsUsers, type: :model do
   describe '#balance' do
     let(:user) { create(:user, :full_user) }
     let(:other_user) { create(:user) }
-    let(:grocery) { user.groceries.last }
+    let(:grocery) { user.user_groups.first.groceries.first }
 
     context 'with only one kit member' do
       it 'should always have a zero balance' do
@@ -34,11 +34,6 @@ RSpec.describe UserGroupsUsers, type: :model do
         end
 
         context 'with pre-existing payments' do
-          before :each do
-            GroceryPayment.create(grocery: grocery, user: user, price: 4)
-            GroceryPayment.create(grocery: grocery, user: other_user, price: 0)
-          end
-
           it 'should have the payer at a more negative balance' do
             GroceryPayment.create(grocery: grocery, user: user, price: 4)
             GroceryPayment.create(grocery: grocery, user: other_user, price: 0)
@@ -51,66 +46,66 @@ RSpec.describe UserGroupsUsers, type: :model do
               expect(user_group_user.balance).to eq balances[user_group_user.user_id]
             end
           end
+        end
+      end
 
-          context 'without user payments' do
-            context 'with each member contributing' do
-              context 'with equal pay' do
-                it 'should have users at a zero balance' do
-                  GroceryPayment.create(grocery: grocery, user: user, price: 4)
-                  GroceryPayment.create(grocery: grocery, user: other_user, price: 4)
+      context 'without user payments' do
+        context 'with each member contributing' do
+          context 'with equal pay' do
+            it 'should have users at a zero balance' do
+              GroceryPayment.create(grocery: grocery, user: user, price: 4)
+              GroceryPayment.create(grocery: grocery, user: other_user, price: 4)
 
-                  grocery.user_group.user_groups_users.each do |user_group_user|
-                    expect(user_group_user.balance).to eq 0.to_money
-                  end
-                end
+              grocery.user_group.user_groups_users.each do |user_group_user|
+                expect(user_group_user.balance).to eq 0.to_money
               end
+            end
+          end
 
-              context 'with unequal pay' do
-                it 'it should determine positive and negative balance' do
-                  GroceryPayment.create(grocery: grocery, user: user, price: 4)
-                  GroceryPayment.create(grocery: grocery, user: other_user, price: 2)
+          context 'with unequal pay' do
+            it 'it should determine positive and negative balance' do
+              GroceryPayment.create(grocery: grocery, user: user, price: 4)
+              GroceryPayment.create(grocery: grocery, user: other_user, price: 2)
 
-                  balances = {}
-                  balances[user.id] = -1.to_money
-                  balances[other_user.id] = 1.to_money
+              balances = {}
+              balances[user.id] = -1.to_money
+              balances[other_user.id] = 1.to_money
 
-                  grocery.user_group.user_groups_users.each do |user_group_user|
-                    expect(user_group_user.balance).to eq balances[user_group_user.user_id]
-                  end
-                end
+              grocery.user_group.user_groups_users.each do |user_group_user|
+                expect(user_group_user.balance).to eq balances[user_group_user.user_id]
               end
             end
           end
         end
-      end
-    end
 
-    context 'with only some members contributing' do
-      it 'should not change the balance of a non-contributor' do
-        GroceryPayment.create(grocery: grocery, user: user, price: 4)
+        context 'with only some members contributing' do
+          it 'should not change the balance of a non-contributor' do
+            GroceryPayment.create(grocery: grocery, user: user, price: 4)
 
-        grocery.user_group.user_groups_users.each do |user_group_user|
-          expect(user_group_user.balance).to eq 0.to_money
+            grocery.user_group.user_groups_users.each do |user_group_user|
+              expect(user_group_user.balance).to eq 0.to_money
+            end
+          end
         end
-      end
-    end
 
-    context 'with multiple kits' do
-      it 'should only include payments from the specified kit' do
-        other_user_group = create(:user_group, :with_groceries)
-        other_user_group.users << [user, other_user]
+        context 'with multiple kits' do
+          it 'should only include payments from the specified kit' do
+            other_user_group = create(:user_group, :with_groceries)
+            other_user_group.users << [user, other_user]
 
-        GroceryPayment.create(grocery: other_user_group.groceries.last, user: other_user, price: 4)
-        GroceryPayment.create(grocery: other_user_group.groceries.last, user: user, price: 0)
-        GroceryPayment.create(grocery: grocery, user: user, price: 4)
-        GroceryPayment.create(grocery: grocery, user: other_user, price: 0)
+            GroceryPayment.create(grocery: other_user_group.groceries.last, user: other_user, price: 4)
+            GroceryPayment.create(grocery: other_user_group.groceries.last, user: user, price: 0)
+            GroceryPayment.create(grocery: grocery, user: user, price: 4)
+            GroceryPayment.create(grocery: grocery, user: other_user, price: 0)
 
-        balances = {}
-        balances[user.id] = -2.to_money
-        balances[other_user.id] = 2.to_money
+            balances = {}
+            balances[user.id] = -2.to_money
+            balances[other_user.id] = 2.to_money
 
-        grocery.user_group.user_groups_users.each do |user_group_user|
-          expect(user_group_user.balance).to eq balances[user_group_user.user_id]
+            grocery.user_group.user_groups_users.each do |user_group_user|
+              expect(user_group_user.balance).to eq balances[user_group_user.user_id]
+            end
+          end
         end
       end
     end
