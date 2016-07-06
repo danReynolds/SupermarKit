5b114b4daadfdbe027602a23812002410eb3082b
danReynolds <fnetma@hotmail.com>
2016-07-06 11:48:14 -0400

    added validation and tests for user payment creation

--- app/models/user_payment.rb
+++ app/models/user_payment.rb
@@ -2,6 +2,8 @@ class UserPayment < ActiveRecord::Base
   belongs_to :user_group
   belongs_to :payee, class_name: User
   belongs_to :payer, class_name: User
+  
   validates :price, numericality: { greater_than_or_equal_to: 0 }
+  validates_presence_of :payee, :payer, :user_group
   monetize :price_cents
 end
