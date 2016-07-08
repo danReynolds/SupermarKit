904efb85d36ca0654180b67e5ee0ccb3289c4555
danReynolds <fnetma@hotmail.com>
2016-07-08 16:33:53 -0400

    fix flow further

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -27,7 +27,7 @@ class UserGroupsController < ApplicationController
        .update_attributes(state: UserGroupsUsers::ACCEPTED)
 
       current_user.update_attribute(:default_group, @user_group) unless current_user.default_group
-      redirect_to new_user_group_grocery_path(@user_group)
+      redirect_to @user_group, notice: 'Kit created! When you are ready, create your first grocery list.'
     else
       @kit_data = kit_data
       @banner_image = UserGroup::BANNER_IMAGES.sample
