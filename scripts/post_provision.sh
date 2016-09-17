e28ca9c33c2ed094fc75be5fbe063ea685d39fea
Dan <me@danreynolds.ca>
2016-09-17 16:37:48 -0700

    configured tests to work in development

--- scripts/post_provision.sh
+++ scripts/post_provision.sh
@@ -1,6 +1,6 @@
 cd /vagrant
 cp config/database.yml.sample config/database.yml
-bin/bundle install
+LOCAL_ENVIRONMENT=development bin/bundle install
 rake db:create
 rake db:migrate
 rails server -p 4000 -b 0.0.0.0
