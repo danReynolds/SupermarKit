9c8f4fefb90daf3ca37655133d5a72fa506af9b7
Dan <me@danreynolds.ca>
2016-09-24 18:55:36 -0700

    add item and receipt information to the payments page

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,8 +7,6 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-
-
 # role-based syntax
 # ==================
 
