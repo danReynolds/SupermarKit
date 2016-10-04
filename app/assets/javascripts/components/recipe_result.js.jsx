c424a2a87b171b3a457bbd8bb07d0232afec7be2
Dan <me@danreynolds.ca>
2016-10-03 22:25:11 -0700

    add additional recipe information to the recipe results

--- app/assets/javascripts/components/recipe_result.js.jsx
+++ app/assets/javascripts/components/recipe_result.js.jsx
@@ -13,10 +13,25 @@ var RecipeResult = React.createClass({
     },
 
     render: function() {
-        var resultClass = 'valign-wrapper recipe-result' + (this.props.resultIndex === this.props.scrollTarget ? ' target' : "");
-        var instructionsLink;
+        let instructionsLink, descriptionContent;
+        let description = '';
+        const {
+            handleAdd,
+            resultIndex,
+            scrollTarget
+        } = this.props;
+        const {
+            url,
+            flavors,
+            image,
+            name,
+            timeInSeconds,
+            rating,
+            course,
+        } = this.props.result;
+        var resultClass = 'valign-wrapper recipe-result' + (resultIndex === scrollTarget ? ' target' : '');
 
-        if (this.props.result.url) {
+        if (url) {
             instructionsLink = (
                 <a
                     className='btn dark'
@@ -28,19 +43,44 @@ var RecipeResult = React.createClass({
                 </a>
             );
         }
+
+        if (flavors && Object.keys(flavors).length) {
+            const keyFlavors = Object.keys(flavors)
+                .sort((f1, f2) => flavors[f2] - flavors[f1]).slice(0, 2)
+            description = `This recipe is mostly ${keyFlavors.join(' and ')}`
+        }
+
+        if (course) {
+            const courseInfo = course.slice(0, 2).join(' or ')
+            if (description) {
+                description += ` and it is a good choice if you are looking for ${courseInfo}.`
+            } else {
+                description += `This recipe is a good choice if you are looking for ${courseInfo}.`
+            }
+        }
+
+        if (description.length) {
+            descriptionContent = (
+                <div className='description'>
+                    {description}
+                </div>
+            )
+        }
+
         return (
             <li
                 className={resultClass}
-                onClick={this.props.handleAdd}
-                data-index={this.props.resultIndex}>
-                <img src={this.props.result.image}/>
+                onClick={handleAdd}
+                data-index={resultIndex}>
+                <img src={image}/>
                 <div className='info'>
-                    <p>{this.props.result.name}</p>
-                    <p>{this.recipeTime(this.props.result.timeInSeconds)}</p>
+                    <p>{name}</p>
+                    <p>{this.recipeTime(timeInSeconds)}</p>
                     <div className='ratings'>
-                        {this.renderRatings(this.props.result.rating)}
+                        {this.renderRatings(rating)}
                     </div>
                 </div>
+                {descriptionContent}
                 {instructionsLink}
             </li>
         );
