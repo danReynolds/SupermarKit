d880a4fcbadad995589b8bbb3085fddc134781a6
Dan <me@danreynolds.ca>
2016-10-17 21:30:53 -0700

    remove timeout in place of custom transitions in materialize

--- app/assets/javascripts/components/itemlist.js.jsx
+++ app/assets/javascripts/components/itemlist.js.jsx
@@ -112,16 +112,10 @@ var ItemList = React.createClass({
             }
         );
         this.saveSelection(updatedModal.selection);
-
-        // Timeout is used for transition sliding animation on removal
-        setTimeout(function() {
-            this.setState({
-                total: this.state.total - item.price,
-                modal: updatedModal
-            }, function() {
-                $('.collection-item').css('transform', 'none');
-            }.bind(this));
-        }.bind(this), 90);
+        this.setState({
+            total: this.state.total - item.price,
+            modal: updatedModal
+        });
     },
 
     handleSave: function(modalSelection) {
