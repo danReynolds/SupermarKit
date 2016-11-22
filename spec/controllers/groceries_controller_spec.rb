4bec2b657e7d930d9ab3708cfe1bf7ab6705ca41
Dan <me@danreynolds.ca>
2016-11-21 20:43:22 -0800

    add missing tests for new slack functionality

--- spec/controllers/groceries_controller_spec.rb
+++ spec/controllers/groceries_controller_spec.rb
@@ -489,6 +489,50 @@ describe GroceriesController, type: :controller do
       expect(grocery.reload.finished?).to eq true
     end
 
+    context 'slack messages' do
+      before :each do
+        @slack_bot = create(:slack_bot, :with_messages)
+        allow(@slack_bot).to receive(:send_message)
+        allow(Grocery).to receive(:find).and_return(grocery)
+        allow(grocery).to receive_message_chain(:save!).and_return(true)
+        allow(grocery).to receive_message_chain(:user_group, :slack_bot).and_return(@slack_bot)
+      end
+
+      context 'with a slackbot' do
+        context 'with a receipt' do
+          it 'should send a receipt and checkout message' do
+            allow(grocery).to receive_message_chain(:receipt, :present?).and_return(true)
+
+            subject
+            expect(@slack_bot).to have_received(:send_message)
+            .with(SlackMessage::SEND_CHECKOUT_MESSAGE, grocery)
+            expect(@slack_bot).to have_received(:send_message)
+            .with(SlackMessage::SEND_GROCERY_RECEIPT, grocery)
+          end
+        end
+
+        context 'without a receipt' do
+          it 'should send a checkout message' do
+            subject
+            expect(@slack_bot).to have_received(:send_message)
+            .with(SlackMessage::SEND_CHECKOUT_MESSAGE, grocery)
+          end
+        end
+      end
+
+      context 'without a slackbot' do
+        it 'should not send any slack messages' do
+          allow(grocery).to receive_message_chain(:user_group, :slack_bot).and_return(nil)
+          subject
+          expect(@slack_bot).to_not have_received(:send_message)
+          .with(SlackMessage::SEND_CHECKOUT_MESSAGE, grocery)
+          expect(@slack_bot).to_not have_received(:send_message)
+          .with(SlackMessage::SEND_GROCERY_RECEIPT, grocery)
+        end
+      end
+    end
+
+
     context 'with every user contributing' do
       it 'should create payments for each user' do
         payment_double = class_double('GroceryPayment').as_stubbed_const
--- spec/factories/slack_bot.rb
+++ spec/factories/slack_bot.rb
@@ -1,10 +1,19 @@
 FactoryGirl.define do
   factory :slack_bot do
+    api_token 'test token'
   end
 
   trait :with_messages do
-    after :create do
-      create_list :slack_message, 1, slack_bot: instance
+    after :create do |instance|
+      CONFIGURABLES[:slack_messages].each do |message|
+        create(
+          :slack_message,
+          slack_bot: instance,
+          format: message[:format],
+          message_type: message[:id],
+          enabled: true,
+        )
+      end
     end
   end
 end
