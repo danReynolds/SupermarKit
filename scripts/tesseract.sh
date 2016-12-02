39ae1ff7de4dd4f5cd3f136c51efb546f7608d6f
Dan <me@danreynolds.ca>
2016-12-01 23:16:39 -0800

    use trained data from fork rather than external repo

--- scripts/tesseract.sh
+++ scripts/tesseract.sh
@@ -3,4 +3,4 @@
 apt-get update
 apt-get install libleptonica-dev libtesseract-dev
 mkdir /usr/local/share/tessdata
-curl -L https://github.com/tesseract-ocr/tessdata/raw/master/eng.traineddata > /usr/local/share/tessdata/eng.traineddata
+curl -L https://github.com/danReynolds/ruby-tesseract-ocr/raw/master/eng.traineddata > /usr/local/share/tessdata/eng.traineddata
