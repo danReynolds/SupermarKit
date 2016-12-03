3608f0f039859b61a96ad7322dbce5a26a9d8523
Dan <me@danreynolds.ca>
2016-12-03 13:34:19 -0800

    rescue facebook already created account error

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -9,25 +9,26 @@ class OauthsController < ApplicationController
   def callback
     provider = auth_params[:provider]
 
-    if @user = login_from(provider)
-      redirect_to root_path, notice: "Welcome back #{@user.name}"
-    else
-      begin
+    begin
+      if @user = login_from(provider)
+        flash[:notice] = "Welcome back #{@user.name}"
+        redirect_back(fallback_location: root_path)
+      else
         @user = create_from(provider)
         @user.activate!
 
         reset_session # protect from session fixation attack
         auto_login(@user)
         redirect_to user_groups_path, notice: "Welcome #{@user.name}! Start by creating your first Kit with the people you want to shop with."
-      rescue Exception => e
-        if e.class == ActiveRecord::RecordNotUnique
-          message = "An account has already been made with your #{provider.humanize} email. Try a different login method."
-        else
-          message = "Our fault! We're unable to create a user with your #{provider.humanize} account."
-        end
-        flash[:notice] = message
-        redirect_back(fallback_location: login_path)
       end
+    rescue Exception => e
+      if e.class == ActiveRecord::RecordNotUnique
+        message = "An account has already been made with your #{provider.humanize} email. Try a different login method."
+      else
+        message = "Our fault! We're unable to create a user with your #{provider.humanize} account."
+      end
+      flash[:notice] = message
+      redirect_to login_path
     end
   end
 end
