f660240c1db772267f328e8955d22a1e668a9fc8
Dan <me@danreynolds.ca>
2016-12-11 12:51:00 -0800

    format name before searching for it when update grocery items

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,7 @@ class Item < ApplicationRecord
   has_many :user_groups, through: :groceries
   has_and_belongs_to_many :recipes
 
-  validates :name, presence: true, uniqueness: true
+  validates :name, presence: true
 
   accepts_nested_attributes_for :groceries_items
 
