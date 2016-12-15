82441e28fcfb2c87329c96bd82f7e7ffa8c0aa14
Dan <me@danreynolds.ca>
2016-12-14 17:02:19 -0800

    refactor receipt show and confirm to receipt controller

--- app/controllers/groceries/receipts_controller.rb
+++ app/controllers/groceries/receipts_controller.rb
@@ -6,20 +6,47 @@ class Groceries::ReceiptsController < ApplicationController
 
   TOTAL_KEYWORDS = ['Total', 'Subtotal', 'Balance', 'Debit tend'].freeze
 
+  def show
+    @receipt_data = {
+      token: form_authenticity_token,
+      url: grocery_receipts_path(@grocery),
+      confirm_url: confirm_receipt_grocery_path(@grocery),
+      checkout_url: checkout_grocery_path(@grocery)
+    }
+  end
+
   def create
     @grocery.update!(receipt: params[:file])
-    match_results = find_matches(
+    match_results = process_matches(
       ReceiptProcessor.new(@grocery.receipt.url).process
     )
     render json: render_matches(match_results)
   end
 
+  def confirm
+    grocery_receipt_params[:matches].each do |match|
+      item = Item.accessible_by(current_ability).find_or_create_by(
+        name: match[:name]
+      )
+      GroceriesItems.create_with(requester: current_user).find_or_create_by(
+        item: item,
+        grocery: @grocery
+      ).update!(price: match[:price])
+    end
+
+    render json: { uploader_id: current_user.id }
+  end
+
   private
 
-  def render_matches(match_results)
+  def grocery_receipt_params
+    params.require(:grocery).permit(matches: [:name, :price])
+  end
+
+  def render_matches(matches)
     # Add new items and update existing items with the determined prices
-    match_results.tap do |results|
-      results[:matches].each do |match|
+    matches.tap do ||
+      matches[:list].each do |match|
         if @grocery.items.where(name: match[:name]).length.zero?
           match.merge!(new: true)
         end
@@ -27,31 +54,43 @@ class Groceries::ReceiptsController < ApplicationController
     end
   end
 
-  def find_matches(processed_receipt)
-    processed_receipt.inject({ matches: [], total: 0 }) do |matches, capture|
-      matches.tap do |acc|
-        matcher = Matcher.new(capture.first.strip.downcase.capitalize)
-
-        # There is a special case for matches to the total price
-        if match = matcher.find_match(TOTAL_KEYWORDS)
-          # Multiple matches for a total keyword favor the largest value
-          acc[:total] = [capture[1].to_f, acc[:total]].max
-        elsif match = matcher.find_match(@grocery.items.pluck(:name)) || matcher.find_match(Item.all.pluck(:name))
-          # Aggregate duplicate items together
-          aggregate_match = acc[:matches].detect { |existing_match| existing_match[:name] == match.result }
-          if aggregate_match
-            aggregate_match.merge!({
-              price: aggregate_match[:price] += capture[1].to_f
-            })
-          else
-            acc[:matches] << {
-              name: match.result,
-              price: capture[1].to_f,
-              similarity: match.similarity
-            }
-          end
-        end
+  def process_matches(processed_receipt)
+    processed_receipt.inject(list: [], total: 0) do |matches, capture|
+      matches.tap do |_|
+        process_match(
+          Matcher.new(capture.first.strip.downcase.capitalize),
+          matches,
+          capture[1].to_f
+        )
       end
     end
+  end
+
+  def process_match(matcher, matches, price)
+    return if total_match(matcher, matches, price)
+    return unless match = matcher.find_match(@grocery.items.pluck(:name)) ||
+      matcher.find_match(Item.accessible_by(current_ability).pluck(:name))
+    return if aggregate_match(match, matches, price)
+
+    matches[:list] << {
+      name: match.result,
+      price: price,
+      similarity: match.similarity
+    }
+  end
+
+  # Aggregate duplicate items together
+  def aggregate_match(match, matches, price)
+    return unless aggregate_match = matches[:list].detect do |existing_match|
+      existing_match[:name] == match.result
+    end
+    aggregate_match[:price] += price
+  end
+
+  # There is a special case for matches to the total price
+  def total_match(matcher, matches, price)
+    return unless matcher.find_match(TOTAL_KEYWORDS)
+    # Multiple matches for a total keyword favor the largest value
+    matches[:total] = [price, matches[:total]].max
   end
 end
