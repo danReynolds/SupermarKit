f268b8fa70a8d379be9fb89bfe06e179ef3e7553
Dan <me@danreynolds.ca>
2017-04-27 15:05:48 -0400

    allow logging in with oauth if already a user

--- app/controllers/oauths_controller.rb
+++ app/controllers/oauths_controller.rb
@@ -10,25 +10,22 @@ class OauthsController < ApplicationController
     provider = auth_params[:provider]
 
     begin
-      if @user = login_from(provider)
-        flash[:notice] = "Welcome back #{@user.name}"
-        redirect_back(fallback_location: root_path)
-      else
-        @user = create_from(provider)
-        @user.activate!
+      unless login_from(provider)
+        user_hash = sorcery_fetch_user_hash(provider).with_indifferent_access
+
+        unless @user = User.find_by_email(user_hash[:user_info][:email])
+          @user = create_from(provider)
+          @user.activate!
+          notice = "Welcome #{@user.name}! Start by creating your first Kit with the people you want to shop with."
+        end
 
         reset_session # protect from session fixation attack
         auto_login(@user)
-        redirect_to user_groups_path, notice: "Welcome #{@user.name}! Start by creating your first Kit with the people you want to shop with."
+        flash[:notice] = notice
       end
+      redirect_back(fallback_location: root_path)
     rescue Exception => e
-      if e.class == ActiveRecord::RecordNotUnique
-        message = "An account has already been made with your #{provider.humanize} email. Try a different login method."
-      else
-        message = "Our fault! We're unable to create a user with your #{provider.humanize} account."
-      end
-      flash[:notice] = message
-      redirect_to login_path
+      redirect_to login_path, notice: "Our fault! We're unable to create a user with your #{provider.humanize} account."
     end
   end
 end
