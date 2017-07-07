a59cf6238a598dbfb394a159e2961818cd2c8b12
Dan Reynolds <me@danreynolds.ca>
2017-07-07 14:09:48 -0400

    create nginx_default network on circle

--- deploy.sh
+++ deploy.sh
@@ -14,5 +14,8 @@ docker build -f Dockerfile.prod -t danreynolds/supermarkit:$DEPLOY_TAG .
 docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker push danreynolds/supermarkit:$DEPLOY_TAG
 
+# Create nginx_default network required to run production config
+docker network create nginx_default
+
 # Deploy to Production
 docker-compose -f docker-compose.yml -f docker-compose.production.yml run app rake docker:deploy
